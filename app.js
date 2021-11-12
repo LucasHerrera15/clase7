5fec91df72ffd6ead1c912d42111bda9e5386894
Your Name <you@example.com>
2021-11-11 21:40:22 -0300

    Agregué nueva tarea

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ let {listarTareas, agregarTarea, borrarTarea, tareasEstado} = require('./funcion
 
 let arreglo_objs_literal = require('./servicios/tareas.json');
 
-/*
+
 let nuevaTarea =    
 {
-    "codigo": 17,
-    "titulo": "Comprobar informacion",
+    "codigo": 18,
+    "titulo": "Realizar esquema",
     "estado": "Pendiente" 
 };
 
 
-agregarTarea(arreglo_objs_literal,nuevaTarea);
-*/
+agregarTarea(arreglo_objs_literal,nuevaTarea); 
 
-//arreglo_objs_literal = borrarTarea(arreglo_objs_literal,4);
+
+//arreglo_objs_literal = borrarTarea(arreglo_objs_literal,18);
 
 //listarTareas(arreglo_objs_literal);
 
 //let tareas_pendientes= tareasEstado(arreglo_objs_literal,"Pendiente");
 //console.log(tareas_pendientes);
 
-
+console.log(arreglo_objs_literal)
 

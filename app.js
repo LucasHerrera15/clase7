e3c18a95a9441c2e7f4ddcb025bc393d3c245c21
Your Name <you@example.com>
2021-11-11 21:20:43 -0300

    Mando el ejercicio de la clase anterior

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-let nombre = "Lucas";
+let {listarTareas, agregarTarea, borrarTarea, tareasEstado} = require('./funciones/funcionesDeTareas');  
+
+let arreglo_objs_literal = require('./servicios/tareas.json');
+
+/*
+let nuevaTarea =    
+{
+    "codigo": 17,
+    "titulo": "Comprobar informacion",
+    "estado": "Pendiente" 
+};
+
+
+agregarTarea(arreglo_objs_literal,nuevaTarea);
+*/
+
+//arreglo_objs_literal = borrarTarea(arreglo_objs_literal,4);
+
+//listarTareas(arreglo_objs_literal);
+
+//let tareas_pendientes= tareasEstado(arreglo_objs_literal,"Pendiente");
+//console.log(tareas_pendientes);
+
+
+
